237a8b02ca749f783f3af2b8640f60137af70b27
Chepaq <116429594+Chepaq@users.noreply.github.com>
2023-05-26 11:31:20 +0200

    gestion partenaires

---
+++ tripking/about.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+/*
+Page Qui sommes-nous ?
+
+*/
+include("./fonctions_start.php");
+setup();
+pagenavbar("about.php");
+?>
+<body>
+
+    <h1 class="my-4 text-center">
+        Qui sommes-nous ?
+        <hr>
+    </h1>
+    <h1 class="my-4 text-center">Liste des partenaires</h1 >
+
+    <div class="container">
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Prénom</th>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Date de naissance</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php
+                    $personnages = json_decode(file_get_contents("data/partenaires.json"), true);
+                    foreach ($personnages as $personnage) {
+                    ?>
+                        <tr>
+                            <th scope="row"><?php echo $personnage['id'] ?></th>
+                            <td><?php echo $personnage['prenom'] ?></td>
+                            <td><?php echo $personnage['nom'] ?></td>
+                            <td><?php echo $personnage['date_naissance'] ?></td>
+                            <td style="border: none"><a href="data/supprimer_personnage.php?id=<?php echo $personnage['id'] ?>" class="btn btn-sm btn-danger material-icons">close</a></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <h2 class="my-4">Ajouter un personnage</h2>
+        <form action="personnages/ajouter_personnage.php">
+            <div class="form-row my-4">
+                <div class="offset-1 col-3">
+                    <input type="text" class="form-control" placeholder="Prénom" name="prenom" required>
+                </div>
+                <div class="col-3">
+                    <input type="text" class="form-control" placeholder="Nom" name="nom" required>
+                </div>
+                <div class="col-3">
+                    <input type="text" class="form-control" placeholder="Date de naissance" name="date_naissance" required>
+                </div>
+                <div class="col-2">
+                    <input type="submit" class="btn btn-success" value="Ajouter">
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+
+    </body>
+    <?php footer(); ?>
+</html>
+
+
+
+
---
+++ tripking/supprimer_personnage copy.php
@@ -0,0 +1,20 @@
+<?php
+/* Auteur : Séverin Messiaen */
+
+if (!isset($_GET["prenom"]) || !isset($_GET["nom"]) || !isset($_GET["date_naissance"])) {
+    echo ("Il manque des arguments !");
+    echo ("<pre>");
+    var_dump($_GET);
+    echo ("</pre>");
+    die();
+}
+
+$personnages = json_decode(file_get_contents("partenaires.json"), true);
+array_push($personnages, array(
+    "id" => end($personnages)['id'] + 1,
+    "nom" => $_GET['nom'],
+    "prenom" => $_GET['prenom'],
+    "date_naissance" => $_GET['date_naissance']
+));
+file_put_contents("partenaires.json", json_encode($personnages));
+header("Location: ../about.php");
