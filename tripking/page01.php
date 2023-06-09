97a13713dbd714afd5b791fc9cae9bbbfc184b8d
Chepaq <116429594+Chepaq@users.noreply.github.com>
2023-06-09 09:04:54 +0200

    add file

--- tripking/page01.php
+++ tripking/page01.php
@@ -42,21 +42,21 @@ $_SESSION['partenaire'] = "michelin"; //TEST avec un partenaire de Michelin
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="./images/louis.png" class="w-100">
+                        <img src="./images/home.png" class="w-100">
                         <div class="carousel-caption d-none d-md-block">
                             <h5>First slide label</h5>
                             <p>Some representative placeholder content for the first slide.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="./images/louis.png" class="w-100">
+                        <img src="./images/mer.png" class="w-100">
                         <div class="carousel-caption d-none d-md-block">
                             <h5>Second slide label</h5>
                             <p>Some representative placeholder content for the second slide.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="./images/louis.png" class="w-100">
+                        <img src="./images/foret.png" class="w-100">
                         <div class="carousel-caption d-none d-md-block">
                             <h5>Third slide label</h5>
                             <p>Some representative placeholder content for the third slide.</p>
